e99333b0b9875e7a59ef6ca62ae50a473dfff989
Poomer <v.nichayapun@gmail.com>
2015-07-21 11:44:33 +0700

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ var awesomeThoughts = " I am Poomer and I am awesome";
+ var funThoughts = awesomeThoughts.replace("awesome", "fun");
+ console.log(funThoughts);
